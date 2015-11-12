595bc8079fe99c99785af40867fa32b9efa1d569
qinghai <noteon@gmail.com>
2015-11-12 08:43:54 +0800

    add dep jquery

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="ace/ace.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
 /// <reference path="lodash/lodash.d.ts" />
 /// <reference path="moment/moment.d.ts" />
 
